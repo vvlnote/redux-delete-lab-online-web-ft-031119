593aa999e8b80c9070908dd0e4f731596b17e676
Joy Lin <vvlnote@hotmail.com>
2019-09-03 22:56:13 +0000

    Done.

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -3,14 +3,13 @@ import Band from './Band';
 import { connect } from 'react-redux';
 
 class Bands extends Component {
-
 	render() {
   	console.log(this.props.bands);
   	let bands = this.props.bands.map((band, i) => {
   		return <Band key={i} id={band.id} name={band.name} deleteBand={this.props.deleteBand}/>
   		})
 		return(
-			<div> 
+			<div>
 			{bands}
 			</div>
 			)
